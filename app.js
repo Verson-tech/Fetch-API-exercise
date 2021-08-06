09b119103b3a122f7cd604b53cf8c7e51192261d
Verson-tech <77786047+Verson-tech@users.noreply.github.com>
2021-08-06 21:03:10 +0300

    wip

--- app.js
+++ app.js
@@ -1,52 +1,17 @@
-const jokes = document.querySelector("jokes");
-const button = document.querySelector('button');
-// button.addEventListener('click',)
+const jokes = document.querySelector("#jokes");
+const button = document.querySelector("button");
 
-const addNewJoke = () => {
-    const newLI = document.createElement('LI');
-    newLI.append(res.data.joke);
-    jokes.append(newLI)
-
-}
+const addNewJoke = async () => {
+  const jokeText = await getDadJoke();
+  console.log(jokeText);
+  // const newLI = document.createElement('LI');
+  // newLI.append(res.data.joke);
+  // jokes.append(newLI)
+};
 const getDadJoke = async () => {
   const config = { headers: { Accept: "application/json" } };
   const res = await axios.get("https://icanhazdadjoke.com/", config);
   return res.data.joke;
 };
+button.addEventListener('click', addNewJoke);
 
-// fetch("https://api.cryptonator.com/api/ticker/btc-usd").then((res) => {
-//   console.log("RESPONSE, waiting to parse", res)
-//    return res.json()
-// })
-// .then(data=>{
-//     console.log("Data parsed...", data)
-//     console.log(data.ticker.price)
-// })
-// .catch (e => {
-//     console.log ("oh, no!!! ERROR!!!", e)
-// })
-// ----------------------------------------
-
-// const fetchBitcoinPrice = async () => {
-//     const res = await fetch("https://api.cryptonator.com/api/ticker/btc-usd")
-//     const data = await res.json()
-//     console.log(data.ticker.price)
-
-// }
-
-// axios.get("https://api.cryptonator.com/api/ticker/btc-usd")
-// .then(res=>{
-//     console.log(res.data.ticker.price)
-// })
-// .catch(err=>{
-//     console.log('Error', err)
-// })
-
-// const fetchBitcoinPrice=async()=>{
-//     try{
-//         const res=await axios.get("https://api.cryptonator.com/api/ticker/btc-usd")
-//         console.log(res.data.ticker.price)
-//     } catch(e){
-//         console.log("ERROR!", e)
-//     }
-// }
